87e6fdbce6f81e6e1135e3bdb6a8d26a7b074977
Jeffrey Benusa <jbenusa@paramoredigital.com>
2017-03-31 21:06:32 -0500

    Dev environment complete

--- wp-content/themes/wp-automation/gulpfile.js
+++ wp-content/themes/wp-automation/gulpfile.js
@@ -1,14 +1,25 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
-var jshint = require('gulp-jshint');
+var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var del = require('del');
+var jshint = require('gulp-jshint');
+var jshintStylish = require('jshint-stylish');
+var sass = require('gulp-sass');
+
+var browserSync = require('browser-sync');
 
 
+//Paths
 var path = '../genave-timber/assets/';
-
 var src = path + 'src/';
 var dist = path + 'dist/';
 var vendor = path + 'vendor/';
+
+gulp.task('delete:dist', function(callback)
+{
+  del([dist], callback);
+});
 
 gulp.task('sass', function () {
 
@@ -20,18 +31,12 @@ gulp.task('sass', function () {
 
 });
 
-gulp.task('js', function () {
-
-  gulp.src(src + 'scripts/*.js')
-
+gulp.task('scripts', function()
+{
+  gulp.src(src + 'scripts/**/*.js')
     .pipe(jshint())
-
-    .pipe(jshint.reporter('fail'))
-
-    .pipe(concat('theme.js'))
-
-    .pipe(gulp.dest(dist + 'js'));
-
+    .pipe(jshint.reporter(jshintStylish))
+    .pipe(gulp.dest(dist + 'scripts'));
 });
 
 gulp.task('img', function() {
@@ -50,6 +55,14 @@ gulp.task('img', function() {
 
 });
 
+gulp.task('default', ['sass', 'scripts'], function()
+{
+  browserSync({
+    notify: false
+  });
 
-
-gulp.task('default', ['sass', 'js']);
+  gulp.watch(src + 'styles/**/*.scss', ['sass']);
+  gulp.watch(src + 'scripts/**/*.js', ['scripts']);
+  gulp.watch(src + 'images/*.svg', ['svg']);
+  gulp.watch(src + 'images/*', ['images']);
+});
